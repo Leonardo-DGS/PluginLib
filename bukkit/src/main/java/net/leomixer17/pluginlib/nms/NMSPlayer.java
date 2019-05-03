1978384b309a49ce5734068968bcb6ff5e44a621
leonardo-dgs <17832817+leonardo-dgs@users.noreply.github.com>
2019-05-03 17:19:21 +0200

    Wrap some methods in NMSPlayer

--- bukkit/src/main/java/net/leomixer17/pluginlib/nms/NMSPlayer.java
+++ bukkit/src/main/java/net/leomixer17/pluginlib/nms/NMSPlayer.java
@@ -1,5 +1,6 @@
 package net.leomixer17.pluginlib.nms;
 
+import net.leomixer17.pluginlib.util.Players;
 import org.bukkit.entity.Player;
 
 public class NMSPlayer {
@@ -9,6 +10,46 @@ public class NMSPlayer {
     public NMSPlayer(final Player player)
     {
         this.player = player;
+    }
+
+    public Player getPlayer()
+    {
+        return this.player;
+    }
+
+    public void sendMessage(final String... messages)
+    {
+        Players.msg(this.getPlayer(), messages);
+    }
+
+    public void sendTitle(final Integer fadeIn, final Integer stay, final Integer fadeOut, final String title, final String subtitle)
+    {
+        Players.sendTitle(fadeIn, stay, fadeOut, title, subtitle, this.getPlayer());
+    }
+
+    public void sendActionBar(final String text)
+    {
+        Players.sendActionBar(text, this.getPlayer());
+    }
+
+    public void clearActionBar()
+    {
+        this.sendActionBar("");
+    }
+
+    public void clearTitle()
+    {
+        this.sendTitle(0, 0, 0, "", "");
+    }
+
+    public void sendTabTitle(String header, String footer)
+    {
+        Players.sendTabTitle(header, footer, this.getPlayer());
+    }
+
+    public int getPing()
+    {
+        return Players.getPing(this.getPlayer());
     }
 
 }
